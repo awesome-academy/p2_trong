4ca51500b91e9086397e39b56c73dbdc604eb00e
= <=>
2020-05-11 17:08:28 +0700

    checkUser

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React , {Suspense} from  'react';
 import ReactDOM from 'react-dom';
 import App from './container/App/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Suspense fallback={(<div>Loading...........</div>)} >
-      <App />
+      <Provider  store = {store} >
+        <App />
+      </Provider>
     </Suspense>
-  </React.StrictMode>,
+  </React.StrictMode>
+  ,
   document.getElementById('root')
 );
 
